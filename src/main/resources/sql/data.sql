5f05cf2d42c289d6bfaedb8c2ae23fec9c62b8fa
hjy5421 <hjy5421@naver.com>
2022-07-11 21:07:52 +0900

    db 초기 데이터 설정 추가

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,7 @@
+insert into user(user_id,user_point) values ("e68abea0-81d2-4b13-9a90-6c1fbb0e754c",0);
+insert into user(user_id,user_point) values ("2555c522-45ff-4f3e-b37c-9f4f8d315b6a",0);
+insert into user(user_id,user_point) values ("bc94c616-6dc7-4d73-ada3-e55814a2f1c5",0);
+
+insert into place(place_id) values ("2fbe21a6-dab8-46dc-9be8-c948699eba80");
+insert into place(place_id) values ("afcca72f-8687-460c-97f4-25ce590d45f5");
+insert into place(place_id) values ("61e7b3da-4f94-4ffc-8638-8fb06eb2e443");
